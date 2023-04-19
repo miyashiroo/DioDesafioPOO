a8c63c063ce849c44f49af3b4166880ae538dd74
miyas <miyashiroo@gmail.com>
2023-04-18 20:12:00 -0400

    Criação classes chaves

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import br.com.dio.desafio.dominio.Curso;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso = new Curso();
+
+        System.out.println(curso);
+    }
+}
